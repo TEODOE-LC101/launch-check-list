f63a0c8d2af4a557b7e3ccadc1071305181fde8f
Angel Soto <85462911+TEODOE-LC101@users.noreply.github.com>
2021-06-11 00:31:55 +0000

    I put the readline-sync on top and put the input.question as the variable.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // FORK this starter file and save it to your own Repl.it account.
 
-// Declare and initialize the 12 variables here:
+const input = require('readline-sync');
+
 let date = "Monday 2021-03-18";
 let time = "10:05:34 AM";
-let astronautCount = 7;
+let astronautCount = input.question("> Enter the number of atronauts: ");
 let astronautStatus = "ready";
 let averageAstronautMassKg= 80.7;
 let crewMassKg = astronautCount * averageAstronautMassKg;
@@ -13,8 +14,6 @@ let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
 let fuelTempCelsius = -225;
 let fuelLevel = "100%";
 let weatherStatus = "clear";
-
-// Write code to generate the LC04 report here:
 
 console.log("----------------------------------------------");
 console.log("> LC04 - LAUNCH CHECKLIST");
@@ -61,10 +60,3 @@ console.log("----------------------------------------------");
 
 
 // BONUS: Use readline-sync to prompt the user to enter the number of astronauts going on the mission.
-const input = require('readline-sync');
-
-let num1 = input.question("> Enter the number of atronauts: ")
-
-console.log(" ");
-console.log("> The number of astronauts on this mission is: " + num1);
-console.log(" ");
